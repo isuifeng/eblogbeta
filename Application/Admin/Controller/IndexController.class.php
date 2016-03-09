c0062db906b0c454b3efaa8bf80ebe2819ca7e27
isuifeng <847751875@qq.com>
2016-03-09 10:58:09 +0800

    登录

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,9 +3,22 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-
-    }
-    public function login(){
     	$this->display();
+    }
+    public function login($username='',$password=''){
+    	if(IS_POST){
+    		//取得登录用户的信息
+    		$map = array();
+    		$map['username'] = $username;
+    		$user = M('admin')->where($map)->select();
+    		//判断密码是否正确
+    		if( is_array($user) && $user[0]['password'] == md5($password)){
+    			$this->success('登录成功！', U('Index/index'));
+    		}else{
+    			$this->error('登录失败',U('Index/login'));
+    		}
+    	}else{
+    		$this->display();
+    	}    	
     }
 }
