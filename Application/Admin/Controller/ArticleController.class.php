541f8206dc99f279c3a26424ff906effc97b999f
isuifeng <847751875@qq.com>
2016-03-10 14:15:05 +0800

    添加文章

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -5,6 +5,21 @@ use Think\Controller;
 class ArticleController extends Controller{
 	//添加新文章
 	public function add(){
-		$this->display();
+		if(IS_POST){
+			$title = I('title');//文章标题
+			$content = I('content');//文章内容
+			$cid = I('category');//文章类型
+			$data = array();
+			$data['title'] = $title;
+			$data['content'] = $content;
+			$data['cid'] = $cid;
+			M('article')->add($data);
+			$this->success('添加成功',U('Index/index'));
+		}else{
+			$content_title = '新增文章';
+			$this->assign('content_title',$content_title);
+			$this->display();
+		}		
 	}
+	
 }
