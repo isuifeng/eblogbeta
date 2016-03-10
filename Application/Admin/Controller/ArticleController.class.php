05d0c927270fcf45616c93ea06b22a77867fa7ce
isuifeng <847751875@qq.com>
2016-03-10 15:36:35 +0800

    编辑文章

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -15,11 +15,33 @@ class ArticleController extends Controller{
 			$data['cid'] = $cid;
 			M('article')->add($data);
 			$this->success('添加成功',U('Index/index'));
-		}else{
-			$content_title = '新增文章';
-			$this->assign('content_title',$content_title);
+		}else{			
 			$this->display();
 		}		
 	}
-	
+	//编辑文章内容
+	public function edit(){
+		if(IS_POST){
+			$title = I('title');//文章标题
+			$content = I('content');//文章内容
+			$cid = I('category');//文章类型
+			$data = array();
+			$data['title'] = $title;
+			$data['content'] = $content;
+			$data['cid'] = $cid;
+			echo $title;
+		}else{			
+			$aid = I('aid');
+			$map = array();
+			$map['aid'] = $aid;
+			$article = M('article')->where($map)->select();			
+			$title = $article[0]['title'];
+			$content = $article[0]['content'];
+			$cid = $article[0]['cid'];
+			$this->assign('title',$title);
+			$this->assign('content',$content);
+			$this->assign('cid',$cid);
+			$this->display();
+		}
+	}
 }
