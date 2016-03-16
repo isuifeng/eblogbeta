6d0c0c88af45653ea3c0ee7f5c218845c8422cfa
isuifeng <847751875@qq.com>
2016-03-16 17:33:39 +0800

    分类

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -12,8 +12,30 @@ class IndexController extends Controller {
         $data = $res['retData'];
         $this->assign('data',$data);
         
+        //文章分类
+        $category = M('category')->select();
+        $this->assign('category',$category);
+
         $this->display();
     }
+    //文章分类展示
+	public function category(){
+		$cid = I('cid');
+		$map = array();
+		$map['cid'] = $cid;
 
+		//获取该分类的文章
+		$article = M('article')->where($map)->select();
+		$this->assign('article',$article);
+		//文章分类
+        $category = M('category')->select();
+        $this->assign('category',$category);
+        //天气api调用
+        $res = weather();
+        $data = $res['retData'];
+        $this->assign('data',$data);
+        
+        $this->display('index');
+	}
     
 }
