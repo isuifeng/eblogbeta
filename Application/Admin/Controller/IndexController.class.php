7b96e41582b1b4571436a8216585e96e8b139909
isuifeng <847751875@qq.com>
2016-03-17 19:50:27 +0800

    关联类型名称

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -4,7 +4,9 @@ use Think\Controller;
 class IndexController extends InitController {
     //后台首页
     public function index(){
-        $article = M('article')->order('create_time desc')->select();
+        //$article = M('article')->order('create_time desc')->select();
+        $article = M('article')->join('RIGHT JOIN eblog_category ON eblog_article.cid = eblog_category.cid')->
+        			order('eblog_article.create_time desc')->select();
         $this->assign('article',$article);
     	$this->display();
     }
