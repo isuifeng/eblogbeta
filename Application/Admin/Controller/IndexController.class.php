24846c268e73fd302995a1641868ed266c4d11a0
isuifeng <847751875@qq.com>
2016-03-17 20:04:33 +0800

    查询bug

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -5,8 +5,10 @@ class IndexController extends InitController {
     //后台首页
     public function index(){
         //$article = M('article')->order('create_time desc')->select();
-        $article = M('article')->join('RIGHT JOIN eblog_category ON eblog_article.cid = eblog_category.cid')->
-        			order('eblog_article.create_time desc')->select();
+        $field = array('aid','eblog_article.cid','title','view','comment','cname','eblog_article.create_time');
+        $article = M('article')->join('RIGHT JOIN eblog_category ON eblog_article.cid = eblog_category.cid')->field($field)
+        			->order('eblog_article.create_time desc')->select();
+        			//print_r($article);
         $this->assign('article',$article);
     	$this->display();
     }
